caddb4b3fe1627233d442505b6a1bd410321d9dd
Kirillr-Sibirski <93882929+Kirillr-Sibirski@users.noreply.github.com>
2023-08-06 21:01:40 +0300

    Remove finish page and combine everything in one page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route }
     from 'react-router-dom';
 import Home from './pages/Homepage.js';
 import Attestation from './pages/Attestation.js'; 
-import Finish from './pages/Finish.js';
 
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
         <Routes>
             <Route exact path='/' element={<Home />} />
             <Route path='/attestation' element={<Attestation />} />
-            <Route path='/finish' element={<Finish />} />
         </Routes>
     </Router>
   );
